2938a748a071157b02da5909128c3b1c07426190
Benedict Tivisini <b.s.tivisini@gmail.com>
2021-05-18 09:07:52 +1000

    candidate table

---
+++ main.cpp
@@ -0,0 +1,88 @@
+//
+//  main.cpp
+//  SWE_Project
+//
+//  Created by Benedict Tivisini on 8/5/21.
+//
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+struct candidate
+{
+    char name[30];          // Name of candidate
+    int age;                // Age of candidate
+    char gender[6];         // Gender of candidate (male/female/other)
+    char electorate[20];    // Electorate (district/region)
+    char party[30];         // Name of the candidate's party
+    char abbreviation[5];   // Abbreviated party name
+    char logo[30];          // Description of party's symbol
+    char officer[30];       // Registered officer of the party
+    int count;              // Current number of votes for candidate
+    int ranking;            // Ranking across candidates (1 being most votes)
+};
+
+
+
+candidate Candidate_1 =
+{
+    
+"Candidate 1",              // name
+0,                          // age
+"",                         // gender
+"",                         // electorate
+"Australian Labor Party",   // party
+"ALP",                      // abbreviation
+"Text: 'Victorian Labour'", // logo
+"Mr Christopher Ford",      // officer
+0,                          // count
+0,                          // ranking
+    
+};
+
+candidate Candidate_2 =
+{
+    
+"Candidate 2",              // name
+0,                          // age
+"",                         // gender
+"",                         // electorate
+"Australian Labor Party",   // party
+"ALP",                      // abbreviation
+"Text: 'Victorian Labour'", // logo
+"Mr Christopher Ford",      // officer
+0,                          // count
+0,                          // ranking
+    
+};
+
+
+
+candidate  =
+{
+    
+        // name
+        // age
+        // gender
+        // electorate
+        // party
+        // abbreviation
+        // logo
+        // officer
+        // count
+        // ranking
+    
+};
+
+
+
+
+int main()
+{
+    
+
+    return 0;
+}

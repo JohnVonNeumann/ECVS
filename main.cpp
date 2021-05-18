c8ab12dacb2e974031cfd62ea263c60d27211077
JohnVonNeumann <louiswillcock@gmail.com>
2021-05-18 23:16:17 +1000

    FIX main.cpp - clean the main.cpp to get the program to compile

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
-#include<iostream>
-#include<fstream>
+#include <fstream>
+#include <iostream>
+
 using namespace std;
-
-
 /*
  * Present menu to user
  * If selection P:
@@ -27,56 +26,42 @@ using namespace std;
 
 char menu();
 
-
-
 int main()
 {
-
-	
-
-
 	char choice;
-
 	choice = menu();
 
 	switch (choice)
 	{
-	case "P":
-
-		break;
-	case "A":
-		break;
-	
-	case "S":
-		break;
-
-	case "L":
-		break;
-
-	case "Q":
-		break;
-
+        case 'P':
+            break;
+        case 'A':
+            break;
+        case 'S':
+            break;
+        case 'L':
+            break;
+        case 'Q':
+            break;
+	    default:
+	        menu();
 	}
-
 }
-
 
 char menu()
 {
-
 	char choice;
-
 	do
 	{
-		cout << "Main menu:" << endl << "Please choose from the following" << endl;
-
+        // create a print_menu
+        cout << "Main menu:" << endl << "Please choose from the following" << endl;
 		cout << "P - Display information on certain candidate" << endl;
 		cout << "A - Add votes to candidate" << endl;
 		cout << "S - Display candidate with smallest amount of votes" << endl;
 		cout << "L - Display candidate with largest number of votes" << endl;
 		cout << "Q - Quit" << endl;
 		cin >> choice;
-	} while (choice != "P" || choice != "A" || choice != "S" || choice != "L" || choice != "Q")
-
-		return choice;
+	} while (choice != 'P' || choice != 'A' || choice != 'S' || choice != 'L' || choice != 'Q'); {
+	    return choice;
+	}
 }

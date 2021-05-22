e5019f93337a0a94444d14d5ba9aa632093e9a76
rubalf <102156112@student.swin.edu.au>
2021-05-22 23:05:31 +1000

    Finished up functioning v1 of main.cpp

--- candidate.cpp
+++ candidate.cpp
@@ -35,25 +35,25 @@ struct CandidateInfo
 
 CandidateInfo candidate[10] = {
     
-    {1,"Candidate 1",48,"Male","Mulgrave","Australian Labor Party","ALP","Text: 'Victorian Labour'.","Mr Christopher Ford",0},
+    {1,"Candidate 1",48,"Male","Mulgrave","Australian Labor Party","ALP","Text: 'Victorian Labour'.","Mr Christopher Ford",2},
     
-    {2,"Candidate 2",41,"Female","Kew","Democratic Labour Party (DLP)","Labour DLP","Text 'LABOUR DLP' surrounded by wheat.","Mr Joel Van Der Horst",0},
+    {2,"Candidate 2",41,"Female","Kew","Democratic Labour Party (DLP)","Labour DLP","Text 'LABOUR DLP' surrounded by wheat.","Mr Joel Van Der Horst",4},
     
-    {3,"Candidate 3",59,"Male","Altona","National Party of Australia – Victoria","The Nationals","Bold text 'THE NATIONALS' followed by softer text 'for Regional Victoria'.","Mr Matthew Harris",0},
+    {3,"Candidate 3",59,"Male","Altona","National Party of Australia – Victoria","The Nationals","Bold text 'THE NATIONALS' followed by softer text 'for Regional Victoria'.","Mr Matthew Harris",3},
     
-    {4,"Candidate 4",55,"Female","Albert Park","Liberal Party of Australia – Victorian Division","Liberal","Australian flag above text 'LIBERAL'.","Mr Sam McQuestin",0},
+    {4,"Candidate 4",55,"Female","Albert Park","Liberal Party of Australia – Victorian Division","Liberal","Australian flag above text 'LIBERAL'.","Mr Sam McQuestin",5},
     
-    {5,"Candidate 5",33,"Male","Box Hill","Australian Labor Party","ALP","Text: 'Victorian Labour'.","Mr Christopher Ford",0},
+    {5,"Candidate 5",33,"Male","Box Hill","Australian Labor Party","ALP","Text: 'Victorian Labour'.","Mr Christopher Ford",4},
     
-    {6,"Candidate 6",37,"Female","Prahran","Liberal Party of Australia – Victorian Division","Liberal","Australian flag above text 'LIBERAL'.","Mr Sam McQuestin",0},
+    {6,"Candidate 6",37,"Female","Prahran","Liberal Party of Australia – Victorian Division","Liberal","Australian flag above text 'LIBERAL'.","Mr Sam McQuestin",8},
     
-    {7,"Candidate 7",40,"Male","Burwood","Liberal Democratic Party","Liberal Democrats","Bold text 'LIBERAL DEMOCRATS'.","Mr Tim Quilty",0},
+    {7,"Candidate 7",40,"Male","Burwood","Liberal Democratic Party","Liberal Democrats","Bold text 'LIBERAL DEMOCRATS'.","Mr Tim Quilty",1},
     
-    {8,"Candidate 8",31,"Female","Hawthorn","The Australian Greens – Victoria","Australian Greens","Bold text 'THE GREENS' within a triangle.","Mr Martin Shield",0},
+    {8,"Candidate 8",31,"Female","Hawthorn","The Australian Greens – Victoria","Australian Greens","Bold text 'THE GREENS' within a triangle.","Mr Martin Shield",7},
     
-    {9,"Candidate 9",51,"Male","Ivanhoe","The Australian Greens – Victoria","Australian Greens","Bold text 'THE GREENS' within a triangle.","Mr Martin Shield",0},
+    {9,"Candidate 9",51,"Male","Ivanhoe","The Australian Greens – Victoria","Australian Greens","Bold text 'THE GREENS' within a triangle.","Mr Martin Shield",10},
     
-    {10,"Candidate 10",46,"Female","Bundoora","Pauline Hanson’s One Nation","PHON","Text 'PAULINE HANSON'S one NATION' with emphasis on 'one'.","officer",0}
+    {10,"Candidate 10",46,"Female","Bundoora","Pauline Hanson’s One Nation","PHON","Text 'PAULINE HANSON'S one NATION' with emphasis on 'one'.","officer",50}
     
 };
 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <fstream>
 #include <iostream>
+#include "voter.cpp"
+#include "candidate.cpp"
 
 using namespace std;
+
 /*
  * Present menu to user
  * If selection P:
@@ -33,41 +36,161 @@ void display_highest_voted_candidate();
 
 int main()
 {
-	char choice;
-	choice = menu();
-
-	switch (choice)
+	while (1)
 	{
-        case 'P':
-            break;
-        case 'A':
-            break;
-        case 'S':
-            break;
-        case 'L':
-            break;
-        case 'Q':
-            break;
-	    default:
-	        menu();
+		char choice;
+		choice = menu();
+
+		switch (choice)
+		{
+		case 'P':
+			print_candidate_numbers();
+			break;
+
+
+		case 'A':
+			add_votes_to_candidate();
+			break;
+
+
+		case 'S':
+			display_lowest_voted_candidate();
+
+			break;
+
+
+
+		case 'L':
+			display_highest_voted_candidate();
+
+			break;
+
+
+		case 'Q':
+			exit(1);
+			break;
+
+
+		default:
+			menu();
+		}
+
 	}
+
+	return 0;
+}
+
+void print_candidate_numbers()
+{
+	int choice;
+
+	cout << "Enter candidate ID: " << endl;
+	cin >> choice;
+	while (choice > 10 || choice < 0)
+	{
+		cout << "Enter candidate ID: " << endl;
+		cin >> choice;
+
+	}
+
+	cout << "Candidate " << candidate[choice - 1].id << " - " << candidate[choice - 1].name << " has " << candidate[choice - 1].count << " votes" << endl << endl;
+}
+
+
+
+void add_votes_to_candidate()
+{
+
+	int votchoice, canchoice;
+
+	cout << "Enter voter ID: " << endl;
+	cin >> votchoice;
+
+	while (votchoice > 10 || votchoice < 0)
+	{
+		cout << "Enter voter ID: " << endl;
+		cin >> votchoice;
+	}
+
+	cout << "Enter candidate ID: " << endl;
+	cin >> canchoice;
+
+	while (canchoice > 10 || canchoice < 0)
+	{
+		cout << "Enter candidate ID: " << endl;
+		cin >> canchoice;
+	}
+
+	voter[votchoice - 1].candidate_selection = canchoice;
+	candidate[canchoice - 1].count += 1;
+
+	cout << candidate[canchoice - 1].name << " has " << candidate[canchoice - 1].count << " votes " << endl;
+
+}
+
+
+void display_lowest_voted_candidate()
+{
+	int index;
+	int temp = candidate[0].count;
+	for (int i = 0; i < 10; i++)
+	{
+		if (temp > candidate[i].count)
+		{
+			temp = candidate[i].count;
+			index = i;
+		}
+
+	}
+	
+	cout << "The lowest amount of votes is " << candidate[index].name << " with " << candidate[index].count << " votes" << endl << endl;
+	
+}
+
+
+void display_highest_voted_candidate()
+{
+	int large, i, index;
+	large = candidate[0].count;
+	for (i = 1; i < 10; i++) {
+		if (candidate[i].count > large) {
+			large = candidate[i].count;
+			index = i;
+		}
+	}
+
+	cout << "The highest amount of votes is " << candidate[index].name << " with " << candidate[index].count << " votes" << endl << endl;
+
 }
 
 
 char menu()
 {
-	char choice;
-	do
-	{
-        // create a print_menu
+	char choice; 
+	// create a print_menu
         cout << "Main menu:" << endl << "Please choose from the following" << endl;
 		cout << "P - Display information on certain candidate" << endl;
 		cout << "A - Add votes to candidate" << endl;
 		cout << "S - Display candidate with smallest amount of votes" << endl;
 		cout << "L - Display candidate with largest number of votes" << endl;
-		cout << "Q - Quit" << endl;
+		cout << "Q - Quit" << endl << endl;
 		cin >> choice;
-	} while (choice != 'P' || choice != 'A' || choice != 'S' || choice != 'L' || choice != 'Q'); {
-	    return choice;
-	}
+		cout << endl << endl;
+
+		while (choice != 'P' && choice != 'A' && choice != 'S' && choice != 'L' && choice != 'Q' && choice != 'p' && choice != 'a' && choice != 's' && choice != 'l' && choice != 'q')
+		{
+			cout << "Unknown Selection, Please Try Again" << endl;
+			cout << "Main menu:" << endl << "Please choose from the following" << endl;
+			cout << "P - Display information on certain candidate" << endl;
+			cout << "A - Add votes to candidate" << endl;
+			cout << "S - Display candidate with smallest amount of votes" << endl;
+			cout << "L - Display candidate with largest number of votes" << endl;
+			cout << "Q - Quit" << endl << endl;
+			cin >> choice;
+			cout << endl << endl;
+		 }
+	
+		choice = toupper(choice);
+	return choice;
+	
 }

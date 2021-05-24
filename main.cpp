8b86eb3357b62757abbfbcff98b1f6f301cf8792
JohnVonNeumann <louiswillcock@gmail.com>
2021-05-24 12:29:05 +1000

    UPDATE main.cpp - add func prototypes and consts for file names

--- main.cpp
+++ main.cpp
@@ -52,8 +52,15 @@ void print_candidate_numbers();
 void add_votes_to_candidate();
 void display_lowest_voted_candidate();
 void display_highest_voted_candidate();
+void write_voters_to_file();
+void write_candidates_to_file();
+void read_voters_from_file();
+void read_candidates_from_file();
 void welcome_banner();
 void print_help();
+
+const string VOTER_FILE = "voter.txt";
+const string CANDIDATE_FILE = "candidate.txt";
 
 int main()
 {

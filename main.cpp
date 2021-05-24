1866713aa436f8e18c62e195b89ae7dbc5ba6710
JohnVonNeumann <louiswillcock@gmail.com>
2021-05-24 22:07:43 +1000

    CREATE main.h - move func prototypes into header file

--- main.cpp
+++ main.cpp
@@ -5,9 +5,12 @@
 
 #include "voter.h"
 #include "candidate.h"
+#include "main.h"
 
 using namespace std;
 
+const string VOTER_FILE = "voter.txt";
+const string CANDIDATE_FILE = "candidate.txt";
 /*
  * Present menu to user
  * If selection P:
@@ -51,20 +54,6 @@ using namespace std;
  */
 
 // Function Prototypes
-char menu();
-void print_candidate_numbers();
-void add_votes_to_candidate();
-void display_lowest_voted_candidate();
-void display_highest_voted_candidate();
-void write_voters_to_file();
-void write_candidates_to_file();
-void read_voters_from_file();
-void read_candidates_from_file();
-void welcome_banner();
-void print_help();
-
-const string VOTER_FILE = "voter.txt";
-const string CANDIDATE_FILE = "candidate.txt";
 
 int main()
 {
---
+++ main.h
@@ -0,0 +1,21 @@
+//
+// Created by lw on 24/5/21.
+//
+
+#ifndef ECVS_MAIN_H
+#define ECVS_MAIN_H
+
+char menu();
+void print_candidate_numbers();
+void add_votes_to_candidate();
+void display_lowest_voted_candidate();
+void display_highest_voted_candidate();
+void write_voters_to_file();
+void write_candidates_to_file();
+void read_voters_from_file();
+void read_candidates_from_file();
+void welcome_banner();
+void print_help();
+
+
+#endif //ECVS_MAIN_H
